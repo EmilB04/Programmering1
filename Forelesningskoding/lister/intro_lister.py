1e66e59341d39338c83129c9a7418795313dc8f1
EmilB04 <emil.berglund@live.no>
2023-09-12 12:00:13 +0200

    Laget forelesningskodument

---
+++ Forelesningskoding/lister/intro_lister.py
@@ -0,0 +1,8 @@
+random_list = ["Europe", 7, ["Ny liste", 23, "med nye elementer"], "utenfor"]
+            #    0       1      2/0     2/1        2/2                3
+print(random_list[2][0])       #Skriver ut "Ny liste"
+
+planeter = ["Merkur", "Venus", "Jorden", "Jupiter", "Saturn", "Uranus"]
+print(planeter[0])
+planeter[0] = "Mars"
+print(planeter[0])
