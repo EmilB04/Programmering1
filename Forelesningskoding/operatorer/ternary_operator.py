ff18b587c467ad11643d3d9b7545b5f0255ea759
EmilB04 <emil.berglund@live.no>
2023-09-12 10:42:18 +0200

    La til dokument med ternary operator

---
+++ Forelesningskoding/operatorer/ternary_operator.py
@@ -0,0 +1,6 @@
+# AUTHOR EMIL BERGLUND #
+
+number = -1
+print(f"{number} is a positive number" if number>0 else f"{number} is a negative number")
+number = 4
+print(f"{number} is a positive number" if number>0 else f"{number} is a negative number")
