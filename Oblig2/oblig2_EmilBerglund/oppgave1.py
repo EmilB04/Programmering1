a92f87719d2ceb90c8daf7be4332bc68527d2b6c
EmilB04 <emil.berglund@live.no>
2023-09-13 22:43:43 +0200

    Fullført Oppgave 1

---
+++ Oblig2/oblig2_EmilBerglund/oppgave1.py
@@ -0,0 +1,15 @@
+# AUTHOR EMIL BERGLUND #
+
+ultimate_answer=True
+while ultimate_answer:
+    try:
+        tall=int(input("Hva er svaret på det ultimate spørsmålet om livet, universet og alle ting? Hint: Det er et tall: "))
+        if tall == 42:
+            print("Det stemmer, meningen med livet er 42!")
+            ultimate_answer = False
+        elif tall < 50 and tall > 30:
+            print("Nærme, men feil.")
+        else:
+            print("FEIL!")
+    except ValueError:
+        print("Her leste du ikke hintet ser jeg... prøv igjen!")
