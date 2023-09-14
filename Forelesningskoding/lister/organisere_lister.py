20af71a02a3503993157919a7a8fd9fa6a1867a7
EmilB04 <emil.berglund@live.no>
2023-09-14 11:42:26 +0200

    La til sortering av lister

---
+++ Forelesningskoding/lister/organisere_lister.py
@@ -0,0 +1,9 @@
+# AUTHOR EMIL BERGLUND #
+
+zoo_animals = ["Panda", "Lion", "Giraffe", "Alligator"]
+
+print(sorted(zoo_animals))
+zoo_animals.sort()
+
+print(sorted(zoo_animals, reverse=True))
+zoo_animals.sort(reverse=True)
