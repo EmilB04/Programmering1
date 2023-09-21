5b62dc794dc2e0daf264d2e5c353b3c431fb9028
EmilB04 <emil.berglund@live.no>
2023-09-21 12:12:45 +0200

    Fullført Oppgave 2

--- Oblig2/oblig2_EmilBerglund/oppgave2.py
+++ Oblig2/oblig2_EmilBerglund/oppgave2.py
@@ -10,7 +10,7 @@ for j in range(9, 101):
         print(j)
 
 
-'''Her er to metoder på hvozrdan man løser oppgaven med en "while-løkke" (selv om det ikke var nødvendig med to)'''
+'''Her er to metoder på hvordan man løser oppgaven med en "while-løkke" (selv om det ikke var nødvendig med to)'''
 # Jeg tolket fra og med 9 til 101 som at 9 skal telles med, men ikke 101. Da skulle det i såfall stått "til og med 101"
 num = 9
 while num <= 101:
