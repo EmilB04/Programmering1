f549ebce7095db6e2adbc224d55eee2594096ce7
EmilB04 <emil.berglund@live.no>
2023-09-21 12:11:56 +0200

    Fullført Oppgave 1

--- Oblig2/oblig2_EmilBerglund/oppgave1.py
+++ Oblig2/oblig2_EmilBerglund/oppgave1.py
@@ -1,12 +1,12 @@
 # AUTHOR EMIL BERGLUND #
 
-ultimate_answer=True
-while ultimate_answer:
+ultimate_answer_loop=True
+while ultimate_answer_loop:
     try:
         tall=int(input("Hva er svaret på det ultimate spørsmålet om livet, universet og alle ting? Hint: Det er et tall: "))
         if tall == 42:
             print("Det stemmer, meningen med livet er 42!")
-            ultimate_answer = False
+            ultimate_answer_loop = False
         elif tall < 50 and tall > 30:
             print("Nærme, men feil.")
         else:
