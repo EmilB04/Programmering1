662cb72fe947bd4a519b6c5de811e0404d57c73c
EmilB04 <emil.berglund@live.no>
2023-09-21 12:18:31 +0200

    Fullført Oppgave  4

--- Oblig2/oblig2_EmilBerglund/oppgave4.py
+++ Oblig2/oblig2_EmilBerglund/oppgave4.py
@@ -1,7 +1,5 @@
 # AUTHOR EMIL BERGLUND #
 
-# Gå gjennom listen "tolken_sine_boker" og legg dem til i den tomme listen hvis de er i Lord of the Rings-trilogien. Skriv så ut innholdet i den nye lista ved hjelp av en for -løkke. Det er flere måter man kan skrive en for -løkke på, forsøk å demonstrere et par forskjellige i denne oppgaven.
-
 # Listen er hentet fra Oppgave 3, etter å ha oppdarert den med "Lord of the Rings" forran de som var i trilogien.
 tolken_sine_boker = [
     "The Hobbit",
@@ -21,6 +19,7 @@ for bok in tolken_sine_boker:
         ny_liste.append(bok)
     else:
         continue
+    
 '''
 # Metode 2
 for i in range(len(tolken_sine_boker)):
