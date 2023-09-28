5cfd80c777e269f931ed0a1017d5e5dfa4d66ee8
EmilB04 <emil.berglund@live.no>
2023-09-28 11:20:25 +0200

    Påbegynt oppgave 1

---
+++ Oblig3/oblig3_EmilBerglund/oppgave1.py
@@ -0,0 +1,21 @@
+# AUTHOR EMIL BERGLUND #
+
+student = {
+    "fornavn"      : "Ola",
+    "etternavn"    : "Nordmann",
+    "favorittkurs" : "Programmering 1"
+}
+
+# 1: Skriv ut studentens fullstendige navn (fornavn og etternavn).
+print(student["fornavn"], student["etternavn"])
+
+# 2: Programmatisk endre studentens favorittkurs til å inkludere kursets emnekode: "ITF10219 Programmering 1"
+student["favorittkurs"] = "ITF10219 " + student["favorittkurs"]  # Metode 1
+# student["favorittkurs"] = "ITF10219 Programmering 1"           # Metode 2
+print(student["favorittkurs"])
+
+# 3: Programmatisk legg til en alder for studenten i dictionarien. Du kan selv velge hva alderen skal være.
+student["alder"] = 18
+print(student["alder"])
+
+print(student)
