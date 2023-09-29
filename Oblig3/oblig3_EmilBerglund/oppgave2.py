fec5fb76719f4327c53fb295bcc43524d40cda31
EmilB04 <emil.berglund@live.no>
2023-09-29 13:19:54 +0200

    Fullført Oppgave 2

--- Oblig3/oblig3_EmilBerglund/oppgave2.py
+++ Oblig3/oblig3_EmilBerglund/oppgave2.py
@@ -1,2 +1,12 @@
 # AUTHOR EMIL BERGLUND #
-#
+# Definer en funksjon som lager en fin utskrift med et tilfeldig generert tall mellom 0 og 100 
+#(husk at du kan benytte random.randrange()). Funksjonen skal ikke ta noen parametere. 
+import random as rd
+
+def fin_utskrift():
+    tilfeldig_generert_tall = rd.randrange(0,100)
+    return (f"**********\n*** {tilfeldig_generert_tall} ***\n**********")
+
+for i in range(5):
+    print(fin_utskrift())
+    print("")
