c2398938f4c816bca9e8de10b70cb377cac9a2c8
EmilB04 <emil.berglund@live.no>
2023-09-29 13:29:54 +0200

    Fullført Oppgave 2

--- Oblig3/oblig3_EmilBerglund/oppgave2.py
+++ Oblig3/oblig3_EmilBerglund/oppgave2.py
@@ -3,10 +3,10 @@
 #(husk at du kan benytte random.randrange()). Funksjonen skal ikke ta noen parametere. 
 import random as rd
 
-def fin_utskrift():
+def tilfeldig_tall():
     tilfeldig_generert_tall = rd.randrange(0,100)
     return (f"**********\n*** {tilfeldig_generert_tall} ***\n**********")
 
 for i in range(5):
-    print(fin_utskrift())
+    print(tilfeldig_tall())
     print("")
