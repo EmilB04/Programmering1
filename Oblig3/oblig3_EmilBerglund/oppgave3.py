a89a115989536f947fdc2f4607956b54f1700215
EmilB04 <emil.berglund@live.no>
2023-09-29 13:28:24 +0200

    Fullført Oppgave 3

--- Oblig3/oblig3_EmilBerglund/oppgave3.py
+++ Oblig3/oblig3_EmilBerglund/oppgave3.py
@@ -1,10 +1,11 @@
 # AUTHOR EMIL BERGLUND #
-
-#Definer en funksjon som heter print_list(). 
-#Denne funksjonen skal ta i mot en liste som parameter, 
-#og printe ut hvert element i denne listen en etter en. 
-#Lag deretter kort liste med dine 3 favorittmatretter, 
-#og kall funksjonen din med denne listen som parameter.
+"""
+Definer en funksjon som heter print_list(). 
+Denne funksjonen skal ta i mot en liste som parameter, 
+og printe ut hvert element i denne listen en etter en. 
+Lag deretter kort liste med dine 3 favorittmatretter, 
+og kall funksjonen din med denne listen som parameter.
+"""
 
 def print_list(liste):
     for element in liste:
