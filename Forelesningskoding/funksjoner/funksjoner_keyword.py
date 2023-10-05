9037a07df8f53b93801ec25ce3b6cd9045737aa6
EmilB04 <emil.berglund@live.no>
2023-10-05 11:42:12 +0200

    Forelesning om keywords i funksjoner

---
+++ Forelesningskoding/funksjoner/funksjoner_keyword.py
@@ -0,0 +1,7 @@
+def skriv_ut_spillkarakter(karakternavn, spill="Super Mario Bros"):
+    print(f"{karakternavn} er en karakter i spillet {spill}")
+
+skriv_ut_spillkarakter("Mario")
+skriv_ut_spillkarakter("Mario", "Mario Kart")
+skriv_ut_spillkarakter(karakternavn="Mario", spill="Super Mario Bros")
+skriv_ut_spillkarakter(spill="Super Mario Bros", karakternavn="Mario")
