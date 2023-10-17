36349aeb2119b0ee1309d8deabb25dcd72d3246b
EmilB04 <emil.berglund@live.no>
2023-10-17 10:43:54 +0200

    Forelesning

--- Forelesningskoding/fil_lesing_skriving/json/jsonLesing.py
+++ Forelesningskoding/fil_lesing_skriving/json/jsonLesing.py
@@ -1,23 +1,29 @@
 import json
 
-planeter = [{"navn": "Merkur", "Tyngdekraft": 3.7},
-            {"navn": "Venus", "Tyngdekraft": 8.87},
-            {"navn": "Jorda", "Tyngdekraft": 9.81},            
-            ]
+planeter = [
+    {"navn": "Merkur", "Tyngdekraft": 3.7},
+    {"navn": "Venus", "Tyngdekraft": 8.87},
+    {"navn": "Jorda", "Tyngdekraft": 9.81}
+]
 
-with open ("forelesningskoding/fil_lesing_skriving/json/planeter.json", "w") as utfil:
+# Skriver JSON-data til fil
+with open("forelesningskoding/fil_lesing_skriving/json/planeter.json", "w") as utfil:
     json.dump(planeter, utfil, indent=4)
 
+# Leser JSON-data fra fil
 with open("forelesningskoding/fil_lesing_skriving/json/planeter.json", "r") as fil:
     planeter_fra_fil = json.load(fil)
     print(planeter_fra_fil)
 
-json_string = '''{
-    "dyr" : "Hund",
-    "navn" : "Fido",
-    "alder" : 5,
-    }    
+json_string = '''
+{
+    "dyr": "Hund",
+    "navn": "Fido",
+    "alder": 5
+}
 '''
+
 print(json_string)
+
 hund = json.loads(json_string)
 print(f"Hunden {hund['navn']} er {hund['alder']} år gammel")
