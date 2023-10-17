beec5b815e9221d31f9af30ae97950816e9ef3a9
EmilB04 <emil.berglund@live.no>
2023-10-17 10:39:15 +0200

    Forelesning

--- Forelesningskoding/fil_lesing_skriving/json/jsonLesing.py
+++ Forelesningskoding/fil_lesing_skriving/json/jsonLesing.py
@@ -7,3 +7,17 @@ planeter = [{"navn": "Merkur", "Tyngdekraft": 3.7},
 
 with open ("forelesningskoding/fil_lesing_skriving/json/planeter.json", "w") as utfil:
     json.dump(planeter, utfil, indent=4)
+
+with open("forelesningskoding/fil_lesing_skriving/json/planeter.json", "r") as fil:
+    planeter_fra_fil = json.load(fil)
+    print(planeter_fra_fil)
+
+json_string = '''{
+    "dyr" : "Hund",
+    "navn" : "Fido",
+    "alder" : 5,
+    }    
+'''
+print(json_string)
+hund = json.loads(json_string)
+print(f"Hunden {hund['navn']} er {hund['alder']} år gammel")
