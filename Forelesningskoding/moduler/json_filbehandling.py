e43874218af56e150fb64631c01c8a7cc4544a44
EmilB04 <emil.berglund@live.no>
2023-10-19 10:49:10 +0200

    Forelesning

--- Forelesningskoding/moduler/json_filbehandling.py
+++ Forelesningskoding/moduler/json_filbehandling.py
@@ -3,4 +3,9 @@ import json_modul
 planet = {"name" : "Mercury", "gravity" : 3.73}
 
 file_name = "planet.json"
-json_modul.skriv_json(planet, file_name)
+json_modul.write_json(planet, file_name)
+
+dictionary_from_file = json_modul.read_json(file_name)
+print(dictionary_from_file)
+
+print(dictionary_from_file["name"])
