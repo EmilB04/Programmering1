c8b9a968a0692f27664691cf201f919426d3edd9
EmilB04 <emil.berglund@live.no>
2023-10-23 18:02:22 +0200

    Fullført Oppgave 1A og B

--- Oblig4/oppgave1.py
+++ Oblig4/oppgave1.py
@@ -15,6 +15,12 @@ class Film:
         self.movieYear = movieYear
         self.movieRating = movieRating
 
+    def get_navn(self):
+        return self.movieTitle
+    def get_år(self):
+        return self.movieYear
+    def get_score(self):
+        return self.movieRating
     def print_info(self):
         return f"{self.movieTitle} was released in {self.movieYear} and has a rating of {self.movieRating}"
 
@@ -22,6 +28,8 @@ inception = Film("Inception", 2010, 8.8)
 theMartian = Film("The Martian", 2015, 8.0)
 joker = Film("Joker", 2019, 8.4)
 
-print(inception.print_info(), theMartian.print_info(), joker.print_info(), sep="\n")
+print(f"{inception.get_navn()} was released in {inception.get_år()} and has a rating of {inception.get_score()}")
+print(f"{theMartian.get_navn()} was released in {theMartian.get_år()} and has a rating of {theMartian.get_score()}")
+print(f"{joker.get_navn()} was released in {joker.get_år()} and has a rating of {joker.get_score()}")
 
-
+print("\n",inception.print_info(), theMartian.print_info(), joker.print_info(), sep="\n")
