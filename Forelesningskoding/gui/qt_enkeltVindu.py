97c0065d1adc65f8d2537faadcbdfe003251c692
EmilB04 <emil.berglund@live.no>
2023-11-02 11:06:08 +0100

    mellomlagring

--- Forelesningskoding/gui/qt_enkeltVindu.py
+++ Forelesningskoding/gui/qt_enkeltVindu.py
@@ -1,4 +1,56 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt6.QtGui import QPixmap
 import sys
 
+class Vindu(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Enkelt vindu") # tittel på "adresselinjen/tittelfeltet"
+        self.resize(300, 300)
+
+        label = QLabel()                    # Kaller på ekstern funksjon som lager en label
+        label.setText("Hello World!")       # Teksten som skal vises på skjermen
+
+        label_img = QLabel()
+        pixmap = QPixmap("squarePants.jpg")
+        label_img.setPixmap(pixmap) 
+
+        layout = QVBoxLayout(label)
+        self.setLayout(layout)
+        layout.addWidget(label)
+        layout.addWidget(label_img)
+
+# Programmet starter her
 app = QApplication(sys.argv) # ALLTID MED
+
+vindu = Vindu()
+vindu.show()
+
+sys.exit(app.exec())    # ALLTID MED
+class Vindu(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Enkelt vindu") # tittel på "adresselinjen/tittelfeltet"
+        self.resize(300, 300)
+
+        label = QLabel()                    # Kaller på ekstern funksjon som lager en label
+        label.setText("Hello World!")       # Teksten som skal vises på skjermen
+        label.setStyleSheet("font-size: 40px;")
+        label_img = QLabel()
+        pixmap = QPixmap("squarePants.jpg")
+        label_img.setPixmap(pixmap) 
+
+        layout = QVBoxLayout(label)
+        self.setLayout(layout)
+        layout.addWidget(label)
+
+# Programmet starter her
+app = QApplication(sys.argv) # ALLTID MED
+
+vindu = Vindu()
+vindu.show()
+
+
+
+
+sys.exit(app.exec())    # ALLTID MED
