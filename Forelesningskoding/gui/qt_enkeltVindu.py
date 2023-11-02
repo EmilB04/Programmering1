d94c191341282ba456c55510e7064f348db122d3
EmilB04 <emil.berglund@live.no>
2023-11-02 10:41:11 +0100

    Opprettet fil

---
+++ Forelesningskoding/gui/qt_enkeltVindu.py
@@ -0,0 +1,14 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+app = QApplication(sys.argv)  # ALLTID MED
+
+vindu = QWidget()
+vindu.setWindowTitle("Hello World!")  # Tittel på vinduet
+vindu.resize(300, 250)
+vindu.move(600, 300)  # Begynner øverst i venstre hjørne (x, y)
+
+vindu.show()
+
+
+sys.exit(app.exec())    # ALLTID MED
