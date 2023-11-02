299c603d1a7ece7c4b50b49a49b75b8a42ff25f1
EmilB04 <emil.berglund@live.no>
2023-11-02 11:57:30 +0100

    Forelesning

--- Forelesningskoding/gui/qt_qvbox.py
+++ Forelesningskoding/gui/qt_qvbox.py
@@ -4,22 +4,25 @@ from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
 class HovedVindu(QWidget):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("QVBoxLayout Test")
-        self.resize(300, 300)
 
         layout = QVBoxLayout()
         self.setLayout(layout)
+        self.setGeometry(200, 200, 300, 400)
 
         button1 = QPushButton("TRYKK HER") # Metode 1
         button2 = QPushButton()            # Metode 2
         button2.setText("TRYKK HER OGSÅ")  # Metode 2
         button3 = QPushButton("IKKE TRYKK HER")
 
+        layout.addStretch(1)
         layout.addWidget(button1)
         layout.addWidget(button2)
         layout.addWidget(button3)
+        layout.addStretch(1)
 
+        layout.setSpacing(20) # Luft mellom elementene
+        layout.setContentsMargins(80,0,80,0) # Margin  rundt layouten
 
         self.show()
 
