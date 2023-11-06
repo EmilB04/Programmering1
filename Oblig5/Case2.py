aac261d41eb82b3dc308c28a8f07c5e0697f6600
EmilB04 <emil.berglund@live.no>
2023-11-06 22:44:08 +0100

    Importert fil

---
+++ Oblig5/Case2.py
@@ -0,0 +1,83 @@
+from datetime import date
+
+car_register = {
+    "toyotaBZ4X": {
+        "brand": "Toyota",
+        "model": "Corolla",
+        "price": 96_000,
+        "year": 2012,
+        "month": 8,
+        "new": False,
+        "km": 163_000
+    },
+    "pugeot408": {
+        "brand": "Pugeot",
+        "model": "408",
+        "price": 330_000,
+        "year": 2019,
+        "month": 1,
+        "new": False,
+        "km": 40_000
+    },
+    "audiRS3": {
+        "brand": "Audi",
+        "model": "RS3",
+        "price": 473_000,
+        "year": 2022,
+        "month": 2,
+        "new": True,
+        "km": 0
+    },
+}
+
+NEW_CAR_REGISTRATION_FEE = 8745
+RENT_CAR_PERCENTAGE = 0.4
+RENT_NEW_CAR__FEE = 1000
+
+
+def print_car_information(car):
+    # Oppgave 3.1
+    pass
+
+
+def create_car(car_register, brand, model, price, year, month, new, km):
+    # Oppgave 3.2
+    pass
+
+
+def get_car_age(car):
+    # Oppgave 3.3
+    pass
+
+
+def next_eu_control(car):
+    # Oppgave 3.4
+    pass
+
+
+def rent_car_monthly_price(car):
+    # Oppgave 3.5
+    pass
+
+
+def calculate_total_price(car):
+    # Oppgave 3.6
+    pass
+
+
+def is_new(car):
+    return car['new']
+
+
+if __name__ == '__main__':
+    create_car(car_register, "Volvo", "V90", 850_000, 2021, 12, True, 0)
+    toyota = car_register['toyotaBZ4X']
+    print_car_information(toyota)
+    print(f"\nThe total price for this {toyota['brand']} {toyota['model']} is {calculate_total_price(toyota)}kr.")
+    print(f"Next EU-control for the {toyota['brand']} {toyota['model']} is {next_eu_control(toyota)}")
+    print(f"If you want to rent the {toyota['brand']} {toyota['model']} the monthly fee will be {rent_car_monthly_price(toyota)}.")
+    audi = car_register['audiRS3']
+    print_car_information(audi)
+    print(f"\nThe total price for this {audi['brand']} {audi['model']} is {calculate_total_price(audi)}kr.")
+    print(f"Next EU-control for the {audi['brand']} {audi['model']} is {next_eu_control(audi)}")
+    print(f"If you want to rent the {audi['brand']} {audi['model']} the monthly fee will be {rent_car_monthly_price(audi)}kr.")
