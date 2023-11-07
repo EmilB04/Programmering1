9fce6941fd4aeb6673ef30cbf916d59af2daae77
EmilB04 <emil.berglund@live.no>
2023-11-07 13:52:37 +0100

    Laget bil og regnet ut alder

--- Oblig5/Case2.py
+++ Oblig5/Case2.py
@@ -36,23 +36,34 @@ RENT_NEW_CAR__FEE = 1000
 
 
 def print_car_information(car):
-    for info in car:
-        print(f"Brand: {car['brand']}")
-        print(f"Model: {car['model']}")
-        print(f"Price: {car['price']}")
-        print(f"Manufactured: {car['year']}-{car['month']}")
-        print(f"Condition: {'New' if car['new'] == True else 'Used'}")
-        print()
+    print(f"Brand: {car['brand']}")
+    print(f"Model: {car['model']}")
+    print(f"Price: {car['price']}")
+    print(f"Manufactured: {car['year']}-{car['month']}")
+    print(f"Condition: {'New' if car['new'] == True else 'Used'}")
+    print()
 
 
 def create_car(car_register, brand, model, price, year, month, new, km):
-    # Oppgave 3.2
-    pass
+    car = {
+        f"{brand}{model}": {
+            "brand": brand,
+            "model": model,
+            "price": price,
+            "year": year,
+            "month": month,
+            "new": new,
+            "km": km
+        }
+    }
+    car_register.update(car)    # Samme som append, bare for dictionaries
 
 
 def get_car_age(car):
-    # Oppgave 3.3
-    pass
+    currentYear = date.today().year
+    carYear = car['year']
+    yearsOld = currentYear - carYear
+    return yearsOld
 
 
 def next_eu_control(car):
@@ -76,11 +87,13 @@ def is_new(car):
 
 if __name__ == '__main__':
     create_car(car_register, "Volvo", "V90", 850_000, 2021, 12, True, 0)
+    
     toyota = car_register['toyotaBZ4X']
     print_car_information(toyota)
     print(f"\nThe total price for this {toyota['brand']} {toyota['model']} is {calculate_total_price(toyota)}kr.")
     print(f"Next EU-control for the {toyota['brand']} {toyota['model']} is {next_eu_control(toyota)}")
     print(f"If you want to rent the {toyota['brand']} {toyota['model']} the monthly fee will be {rent_car_monthly_price(toyota)}.")
+    
     audi = car_register['audiRS3']
     print_car_information(audi)
     print(f"\nThe total price for this {audi['brand']} {audi['model']} is {calculate_total_price(audi)}kr.")
