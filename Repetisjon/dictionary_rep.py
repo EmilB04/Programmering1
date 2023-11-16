1c308635c6a66f041c2601461f998d3324ee5c48
EmilB04 <emil.berglund@live.no>
2023-11-16 12:01:06 +0100

    Forelesning

---
+++ Repetisjon/dictionary_rep.py
@@ -0,0 +1,8 @@
+def make_album(artist, title):
+    album = {"artist" : artist, "title" : title}
+    return album
+    
+justin_beaver_album = make_album("Justin Beaver", "Baby")
+
+print(justin_beaver_album)
+
---
+++ Repetisjon/lokker_rep.py
@@ -0,0 +1,11 @@
+for tall in range(5):
+    print(tall)
+    
+    for tall2 in range(3):
+        print(tall2)
+        
+# 0  0 1 2
+# 1  0 1 2
+# 2  0 1 2
+# 3  0 1 2
+# 4  0 1 2
